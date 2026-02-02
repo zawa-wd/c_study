85b53aab26f62c78f9a3a10eb2c988fbb6ca9bdb
zawa <zawa.gmail.com>
2026-02-02 16:20:34 +0900

    Complete Triple Modular Redundancy logic for sensor fusion

--- practice/sensor_fusion.c
+++ practice/sensor_fusion.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/*--- 定数設定 ---*/
 #define MAX_DIFF 10.0           //平均値が左数値以上はErrorとする
 #define ERROR_THRESHOLD 5       //Errorが連続して左の数回あった場合、故障とみなす
 #define BUFFER_SIZE 3           //構造体のサイズ
@@ -16,6 +17,13 @@ typedef struct{
 
 } Sensor;
 
+/*--- プロトタイプ宣言 ---*/
+double update_sensor(Sensor *s,double val);         //センサーのアップデート
+int check_sensor_status(Sensor *s,double raw_diff); //センサーの故障判断
+
+
+/*--- 関数 ---*/
+
 /*************
 *センサーのアップデートをおこなう関数
 * 　引数:double val(センサー情報)
@@ -29,7 +37,6 @@ double update_sensor(Sensor *s,double val){
     double sum = 0;
     for (int i = 0; i < s->count; i++){
         sum += s->buffer[i];
-
     }
     return sum / s->count;
 }
@@ -63,55 +70,81 @@ int check_sensor_status(Sensor *s, double raw_diff){
 int main(){
     Sensor wheel_speed = {{0}, 0, 0, 0};   //ホイールの回転数で車速を確認している想定
     Sensor gps_speed = {{0}, 0, 0, 0};     //GPS情報を利用して車速を確認している想定
-    double w_in = 0, g_in = 0;
-    double raw_diff = 0,avg_diff = 0,avg_w = 0,avg_g = 0; 
+    Sensor accel_speed = {{0}, 0, 0, 0};    //加速度から計算した速度を想定
+    double w_in = 0, g_in = 0, a_in = 0;    //入力した値
+    double raw_diff = 0,avg_diff = 0,avg_w = 0,avg_g = 0,avg_a = 0;//平均値
+    double fusion_speed =0; //合計したスピード
 
     printf("センサー入力を開始：\n");
-    wheel_speed.error_count = 0;
+
     while(1){
-        printf("> ");
-        if(scanf("%lf %lf", &w_in,&g_in) != 2) break; //2種類のセンサーデータを入力
+        printf(">>\n");
+        if(scanf("%lf %lf %lf", &w_in, &g_in, &a_in) != 3) break; //3種類のセンサーデータを入力
 
-        raw_diff = (w_in > g_in) ? (w_in - g_in) : (g_in - w_in);//生データの速度差を計算
+        double diff_wg = (w_in > g_in) ? (w_in - g_in) : (g_in - w_in);
+        double diff_ga = (g_in > a_in) ? (g_in - a_in) : (a_in - g_in);
+        double diff_aw = (a_in > w_in) ? (a_in - w_in) : (w_in - a_in);
 
-        if(check_sensor_status(&wheel_speed, raw_diff) == 1){
-            printf("[WARNING]：ホイールセンサー故障！\n");
-            break;
-        }    
-        if(check_sensor_status(&gps_speed, raw_diff) == 1){
-            printf("[WARNING]：GPSセンサー故障！\n");
+        if(diff_wg <= MAX_DIFF){
+            //WHEELとGPSが近いならその平均を信じる
+            fusion_speed = (w_in + g_in) /2.0;
+            // Accelだけが仲間外れなので、aにだけ「ズレ(diff_a)」を教えてあげる
+            double diff_a = (fusion_speed > a_in) ? (fusion_speed - a_in) : (a_in - fusion_speed);
+    
+            check_sensor_status(&wheel_speed, 0);      // 潔白
+            check_sensor_status(&gps_speed, 0);        // 潔白
+            // 以下疑いあり
+            //diff_aのチェック結果より1が返ってきていたら故障判定
+            if(check_sensor_status(&accel_speed, diff_a) == 1){
+                printf("[WARNING]：ACCELセンサー故障！\n");
+                break;
+            }
+        }
+        else if(diff_ga <= MAX_DIFF){
+            //GPSとaccelが近いならその平均を信じる
+            fusion_speed = (g_in + a_in) /2.0;
+            // Wheelだけが仲間外れなので、wにだけ「ズレ(diff_w)」を教えてあげる
+            double diff_w = (fusion_speed > w_in) ? (fusion_speed - w_in) : (w_in - fusion_speed);
+    
+            check_sensor_status(&gps_speed, 0);        // 潔白
+            check_sensor_status(&accel_speed, 0); // 潔白
+            // 以下疑いあり
+            //diff_wのチェック結果より1が返ってきていたら故障判定
+            if(check_sensor_status(&wheel_speed, diff_w) == 1){
+                printf("[WARNING]：WHEELセンサー故障！\n");
+                break;
+            }
+        }
+        else if(diff_aw <= MAX_DIFF){
+            //accelとwheelが近いならその平均を信じる
+            fusion_speed = (a_in + w_in) /2.0;
+            // GPSだけが仲間外れなので、gにだけ「ズレ(diff_g)」を教えてあげる
+            double diff_g = (fusion_speed > g_in) ? (fusion_speed - g_in) : (g_in - fusion_speed);
+    
+            check_sensor_status(&wheel_speed, 0);      // 潔白
+            check_sensor_status(&accel_speed, 0); // 潔白
+            // 以下疑いあり
+            //diff_gのチェック結果より1が返ってきていたら故障判定
+            if(check_sensor_status(&gps_speed, diff_g) == 1){
+                printf("[WARNING]：GPSセンサー故障！\n");
+                break;
+            }
+        }
+        else{
+            //全ての値(GPW,WHEEL,ACCLEの値)がバラバラ
+            printf("[WARNING]:正しい値が不明です\n");
             break;
         }
-        if(raw_diff <= MAX_DIFF){
-        //生データの時点で速度差がMAX_DIFF以下の場合は使えるデータ化
-            avg_w = update_sensor(&wheel_speed, w_in);
-            avg_g = update_sensor(&gps_speed, g_in);
-            avg_diff = (avg_w > avg_g) ? (avg_w - avg_g) : (avg_g - avg_w);
-        }
+
+        //上記で問題ないことが判断できたため、アップデートしていく
+        avg_w = update_sensor(&wheel_speed, w_in);
+        avg_g = update_sensor(&gps_speed, g_in);
+        avg_a = update_sensor(&accel_speed, a_in);
 
         //構造体内の平均情報をそれぞれ出力
-        printf(" [Average] Wheel:%.2f, GPS: %.2f\n", avg_w, avg_g);
+        printf(" [Average] Wheel:%.2f, GPS: %.2f, Accel: %.2f\n", avg_w, avg_g, avg_a);
 
-        //構造体内の平均での差を確認する
-        double diff = avg_w - avg_g;
-        
-        if(diff < 0) diff = -diff;
-
-        //RAWデータでの差分が10以上であった場合、WARNINGとして警告
-        if(raw_diff > MAX_DIFF){
-            printf("[WARNING][raw_diff](%.2f)\n",raw_diff);
-        }
-
-        //構造体内の平均でも差が5以上ある場合はWARNINGとして警告
-        if(avg_diff > 5.0){
-            printf("[WARNING][avg_diff](%.2f)\n",avg_diff);
-        }
-
-        //WARNING以外の場合、統合速度を出力
-        else{
-            printf("[FUSION]:統合速度＝%.2f km/h\n",avg_diff);
-            printf("[FUSION]:RAW統合＝%.2f km/h\n",raw_diff);
-        }
+        printf("[FUSION] 統合速度：%.2f km/h\n", fusion_speed);
     }
     return 0;
 }
